34ad56ee4fe1dfbaed549c245d7cf90dc480992f
phae <phae@ticketfire.com>
2021-02-12 15:55:05 -0500

    new stlyes and icons, color palette

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,27 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import link from "../../images/link.png";
 import "./styles.css";
 
 const Project = (props) => {
-  const [languages, setLanguages] = useState({});
-  const [project, setProject] = useState('');
-
-  useEffect(() => {
-    setProject(Object.keys(props.project)[0]);
-    setLanguages();
-  }, []);
+  const [show, setShow] = useState(false);
 
   return (
-    <div className="project" key={props.id}>
-      <a href={props.project.url} className="flex space-between">
-        <div className="project">{props.project.name}</div>
-        <div className="languages">
-          {
-            props.project.languages.map((el, i) => {
-              return (<span className="lang" key={i}>{el}</span>);
-            })
-          }
+    <div
+      key={props.id}
+      onMouseEnter={() => {
+        setShow(true);
+      }}
+      onMouseLeave={() => {
+        setShow(false);
+      }}
+    >
+      <div className="project flex space-between">
+        <div className="align-center flex">
+          {props.project.name}
+          <a href={props.project.url}>
+            <img src={link} className="pl-1 link" />
+          </a>
         </div>
-      </a>
+        <div className="languages">
+          {props.project.languages.map((el, i) => {
+            return (
+              <span className="lang" key={i}>
+                {el}
+              </span>
+            );
+          })}
+        </div>
+      </div>
+      <div className={`desc ${show ? "expanded" : ""}`}>
+        {props.project.notes}
+      </div>
     </div>
   );
 };
